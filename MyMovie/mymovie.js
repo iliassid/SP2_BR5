0cb2ac28cb3bb80d7208b8e648a851740922d76c
iliassid <enaailiass@gmail.com>
2025-01-30 11:26:24 +0100

    new commit

---
+++ MyMovie/mymovie.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", loadList);
+
+const nameInput = document.getElementById("nameInput");
+const yearInput = document.getElementById("yearInput");
+const directorInput = document.getElementById("directorInput");
+const tailerInput = document.getElementById("tailerInput");
+const ratingInput = document.getElementById("ratingInput");
+const selectList = document.getElementById("selectList");
+const image = document.getElementById("image");
+const submitBtn = document.getElementById("submitBtn");
